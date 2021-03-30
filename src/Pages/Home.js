a4028aad280b6df3b18c707885274d89fd334153
Alex Berumen <alexandroberumen@gmail.com>
2021-03-29 19:00:25 -0700

    finished up most of news section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,11 @@ import React from 'react'
 import HeroVid from '../Components/HeroVid/HeroVid'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
+import Ronald from '../Images_or_Vids/Images/ronald.jpeg'
+import mbappe from '../Images_or_Vids/Images/mbappe.jpeg'
+import DeBruyn from '../Images_or_Vids/Images/deBruyn.jpeg'
+
 
 
 const HomeContainer = styled.div`
@@ -29,7 +34,7 @@ const NewsTitle = styled.div`
 z-index: 5;
 position: relative;
 width:50rem;
-top: -19rem;
+top: -20rem;
 left: 12rem;
 color:#FF4655;
 font-size: 12rem;
@@ -41,29 +46,33 @@ padding:0;
 const NewsCards = styled.div`
 display: flex;
 position: absolute; 
-top: 135%;
-left: 50%;
+top: 134%;
+left: 50.7%;
 transform: translate(-50%, -50%);
 `
 
 const Card = styled.div`
+position: relative;
+top: 5rem;
 width: 38rem;
-height: 20rem;
-background-color: black;
+height: 30rem;
 margin: 2rem;
 z-index: -1;
+cursor: pointer;
 `
 const NewsButton = styled.button`
 cursor: pointer;
+background: transparent;
 font-family: 'Lato';
 font-weight: 700;
 position: absolute;
-top: 115%;
-left: 75%;
+top: 102%;
+padding: 10rem;
+left: 70%;
 color: #FF4655;
 text-transform: uppercase;
 border: 0px solid transparent;
-transition: 2s ease-in-out;
+transition: .5s ease-in-out;
 
 &:active{
     outline: none;
@@ -74,33 +83,72 @@ transition: 2s ease-in-out;
     outline: none;
 }
 
-&:hover{
-    transform: scaleX(5rem);
-}
+
 `
-const PicImg = styled.img``
-const Date = styled.span`
-color: red;
-position: relative;
-top: 22rem;
+
+
+const PicImg = styled.img`
+object-fit: cover;
+width: 38rem;
+height: 20rem;
+
 `
-const Team = styled.div`
-color: red;
+const Dates = styled.span`
+color: #383E39;
 position: relative;
-top: 22rem;
+top: 2rem;
+font-size: 1rem;
+letter-spacing: .1rem;
+font-weight: 700;
+`
+const Team = styled.span`
+position: relative;
+top: 2rem;
+left: 3rem;
+color: red;
+font-size: 1rem;
+font-weight: 700;
+letter-spacing: .1rem;
+
+
+
 `
 const Headline = styled.div`
-color: red;
+color: #383E39;
 position: relative;
-top: 22rem;
+top: 4rem;
+font-size: 2rem;
+font-weight: 700;
+
+`
+
+const RightArrowIcon = styled(BsArrowRight)`
+position: absolute;
+margin: 10rem;
+top: -.6rem;
+z-index: 15;
+left: 40%;
+transition: 1s ease-in;
+font-size: 3rem;
 
 `
 
 
 
+let Today = new Date();
+let dd = String(Today.getDate()).padStart(2, '0');
+let mm = String(Today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = Today.getFullYear();
+Today = mm + '/' + dd + '/' + yyyy;
+let yesterday = mm + '/' + (dd - 1) + '/' + yyyy
+let twoDaysAgo = mm + '/' + (dd - 2) + '/' + yyyy
 
 const Home = () => {
+
     return (
+
+
+
 
         <HomeContainer>
             <HeroVid />
@@ -108,26 +156,26 @@ const Home = () => {
                 <BackText>Siuuuuu!</BackText>
                 <NewsTitle>Latest News</NewsTitle>
                 <Link to='news'>
-                    <NewsButton>go to news page</NewsButton>
+                    <NewsButton>go to news page <RightArrowIcon /></NewsButton>
                 </Link>
                 <NewsCards>
                     <Card>
-                        <PicImg />
-                        <Date>3</Date>
-                        <Team>4</Team>
-                        <Headline>5</Headline>
+                        <PicImg src={Ronald} />
+                        <Dates>{Today}</Dates>
+                        <Team>Portugal</Team>
+                        <Headline>Ronaldo, Lewandowski, Kane: Who will win the Euro 2021 Golden Boot?</Headline>
                     </Card>
                     <Card>
-                        <PicImg />
-                        <Date>3</Date>
-                        <Team>4</Team>
-                        <Headline>5</Headline>
+                        <PicImg src={mbappe} />
+                        <Dates>{yesterday}</Dates>
+                        <Team>France</Team>
+                        <Headline>Mbappe to decide on his future after Euros</Headline>
                     </Card>
                     <Card>
-                        <PicImg />
-                        <Date>3</Date>
-                        <Team>4</Team>
-                        <Headline>5</Headline>
+                        <PicImg src={DeBruyn} />
+                        <Dates>{twoDaysAgo}</Dates>
+                        <Team>Belgium</Team>
+                        <Headline>Kevin De Bruyne, Belgium Crush Scotland in Euro 2020 Qualifying</Headline>
                     </Card>
 
 
