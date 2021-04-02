9fbabc506a65e80881b4eae1c1c80cc2a56e86f1
Alex Berumen <alexandroberumen@gmail.com>
2021-04-01 18:42:35 -0700

    w

--- src/Pages/Players.js
+++ src/Pages/Players.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+const MainContainer = styled.div`
+position: relative;
+background-color:#0E1A25;
+width: 100%;
+height: 100rem;
+top: 7rem;
+color: #FFFFFF;
+display: flex;
+`
+
+const PlayerNames = styled.ul`
+position: absolute;
+left: 50%;
+top: 20%;
+transform: translate(-50%, -50%);
+text-align: center;
+list-style: none;
+font-family: 'Tungsten';
+font-size: 10rem;
+`
+
+const Player = styled.li``
+
 
 const Players = () => {
     return (
         <div>
-            Players
+            <MainContainer>
+                <PlayerNames>
+                    <Player>1</Player>
+                    <Player>2</Player>
+                    <Player>3</Player>
+                    <Player>4</Player>
+                    <Player>5</Player>
+                    <Player>6</Player>
+                    <Player>7</Player>
+                </PlayerNames>
+            </MainContainer>
         </div>
     )
 }
